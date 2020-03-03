1af10c149ea33fd909505daa7d58b6bd68ed8762
fengjiangtao <fengjiangt@139.com>
2020-03-03 21:29:57 +0800

    项目初始化

---
+++ src/main/java/cn/jeremy/stock/WechatPublicNumberApplication.java
@@ -0,0 +1,13 @@
+package cn.jeremy.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatPublicNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatPublicNumberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/jeremy/stock/WechatPublicNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.jeremy.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatPublicNumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
